e507d61857a2dec69097b9d7754300af0f82a5bd
unknown <sashopirnarev@gmail.com>
2019-12-01 10:35:35 +0200

    add car, navbar components

---
+++ src/components/navbar.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar{" "}
+        <span className="badge badge-pill badge-secondary">
+          <a href="/home">Home</a>
+        </span>
+        <span className="badge badge-pill badge-secondary">
+          <a href="/car">Car</a>
+        </span>
+      </a>
+    </nav>
+  );
+};
+
+export default NavBar;
